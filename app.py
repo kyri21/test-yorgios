fe646ca5397633d6daed290a0b97cc39df23242c
kyri21 <kyriarthur@gmail.com>
2025-04-15 13:27:59 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,3 @@
 import streamlit as st
-
-st.set_page_config(page_title="Test Railway", layout="centered")
-st.title("🚀 Yorgios App déployée sur Railway")
-st.success("Ça fonctionne ! 🎉")
+st.title("✅ Test Railway")
+st.write("Si tu vois ceci, c’est que Railway fonctionne !")
